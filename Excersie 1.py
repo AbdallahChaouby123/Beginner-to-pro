d8e5d516460336a414ffbd8db8b04b48ffdf986a
AbdallahChaouby123 <48841601+AbdallahChaouby123@users.noreply.github.com>
2024-09-05 00:37:31 +0100

    Add files via upload

---
+++ Excersie 1.py
@@ -0,0 +1,8 @@
+Name = input("Please enter your name: ")
+Age = int(input("Please enter your age"))
+new_Age = 2024 - Age
+newnewAge = new_Age + int(100)
+Copies = print (f' Hello , {Name}, "you will turn 100 in the year ", {newnewAge}')
+Copie = int(input("how many copies would you like?"))
+for _ in range(Copie):
+    print (Copies)
